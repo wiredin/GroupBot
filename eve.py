3408a700c2a256caa2c66af2dc6e4f003a8e853e
Mike Weiss <michaelcweiss@gmail.com>
2012-10-26 22:42:02 -0300

    Update eve.py

--- eve.py
+++ eve.py
@@ -3,7 +3,7 @@ import sys
 
 if sys.argv[1]:
     eve = groupbotlib.groupbot("Eve")
-    eve.connectGmail("eve.goupbot@gmail.com","17324430645.17019220200.fn7rGcMgCG@txt.voice.google.com","eve.goupbot","nanat103")
+    eve.connectGmail("eve.goupbot@gmail.com","17324430645.17019220200.fn7rGcMgCG@txt.voice.google.com","eve.goupbot","")
     eve.setMessage(sys.argv[1])
     eve.sendMessage()
     eve.disconnectGmail()
